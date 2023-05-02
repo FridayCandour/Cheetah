f7052643057c18d8321ec0f0e8ebbe52e0b15a1f
fridaycandour <fridaymichaels662@gmail.com>
2023-05-02 22:54:07 +0100

    check /tests

--- src/index.js
+++ src/index.js
@@ -1,18 +1,104 @@
-import { unlinkSync, mkdirSync, existsSync } from "fs";
-import { backupFiles, getFile, createFile, updateFile } from "./fn.js";
+import tar from "tar";
+import glob from "glob";
+import {
+  openSync,
+  readFileSync,
+  writeFileSync,
+  unlinkSync,
+  closeSync,
+  ftruncateSync,
+  readdirSync,
+  copyFileSync,
+  mkdirSync,
+  rmSync,
+  existsSync,
+  // @ts-ignore
+} from "fs";
 
-export class cheetah {
-  cheetahDirectory = "CheetahDB";
-  cacheSize = 100;
+import pkg from "fs-ext";
+const { flockSync } = pkg;
+
+async function createFile(file) {
+  if (!existsSync(file)) {
+    writeFileSync(file, "{}", { flag: "w", encoding: "utf8" });
+    return true;
+  }
+  return false;
+}
+
+async function backupFiles(source) {
+  const backup = source + "/.backup";
+  // copying
+  readdirSync(source).forEach((file) => {
+    if (file !== ".backup") {
+      copyFileSync(`${source}/${file}`, `${backup}/${file}`);
+    }
+  });
+  // backing up
+  const filePattern = `${source}/*.json`;
+  const backupFile = "backup.tar.gz";
+  const files = await glob(filePattern);
+  tar.create({ file: backupFile, cmd: source, gzip: true }, files);
+  // deleting copied files
+  rmSync(backup, { recursive: true, force: true });
+  mkdirSync(backup);
+}
+
+function getFile(filename) {
+  try {
+    return JSON.parse(readFileSync(filename, "utf-8"));
+  } catch (error) {
+    return undefined;
+  }
+}
+
+function updateFile(filename, data) {
+  // Acquire an exclusive lock on the file
+  const fd = openSync(filename, "r+");
+  let existingData = {};
+  try {
+    flockSync(fd, "ex");
+    // Read the existing data from the file
+    try {
+      const fileContents = readFileSync(filename, "utf-8");
+      existingData = JSON.parse(fileContents);
+    } catch (err) {
+      // Ignore errors if the file is empty or does not contain valid JSON data
+    }
+
+    // Update the data
+    Object.assign(existingData, data);
+
+    // Write the updated data to a temporary file
+    const tmpFilename = `${filename}.tmp`;
+    writeFileSync(tmpFilename, JSON.stringify(existingData));
+
+    // Replace the original file with the updated data
+    ftruncateSync(fd, 0);
+    writeFileSync(fd, readFileSync(tmpFilename));
+
+    // Remove the temporary file
+    unlinkSync(tmpFilename);
+  } finally {
+    // Release the file lock
+    flockSync(fd, "un");
+    closeSync(fd);
+  }
+
+  return existingData;
+}
+
+export default class cheetah {
+  cheetahDirectory;
+  cacheSize;
   cache = {};
   constructor(init) {
-    const { Directory, cacheSize } = typeof init === "object" ? init : {};
-    if (typeof Directory === "string") {
-      this.cheetahDirectory = Directory;
-    }
-    if (typeof cacheSize === "number") {
-      this.cacheSize = cacheSize;
-    }
+    const { Directory, cacheSize } =
+      typeof init === "object"
+        ? init
+        : { cacheSize: 100, Directory: "CheetahDB" };
+    this.cheetahDirectory = Directory;
+    this.cacheSize = cacheSize;
     // operations
     try {
       mkdirSync(this.cheetahDirectory);
@@ -22,18 +108,16 @@ export class cheetah {
   backup() {
     return backupFiles(this.cheetahDirectory);
   }
+
   update(key, value) {
-    updateFile(`${this.cheetahDirectory}/${key}.json`, value);
+    const update = updateFile(`${this.cheetahDirectory}/${key}.json`, value);
     if (Object.keys(this.cache).length < this.cacheSize) {
-      if (this.cache[key]) {
-        Object.assign(this.cache[key], value);
-      } else {
-        this.cache[key] = value;
-      }
+      this.cache[key] = update;
     } else {
       delete this.cache[Object.keys(this.cache).length - 1];
       this.cache[key] = value;
     }
+    return update;
   }
   get(key) {
     if (this.cache[key]) {
