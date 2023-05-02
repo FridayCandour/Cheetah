dfa61b28306239e24f055c9f132606740be97ab5
fridaycandour <fridaymichaels662@gmail.com>
2023-05-02 22:56:33 +0100

    removed a ` at line 16

--- tests/http.js
+++ tests/http.js
@@ -7,17 +7,18 @@ const cheetahR = new cheetah();
 await cheetahR.create("foo");
 let counter = 0;
 //
-createServer(handler).listen(httpPort, start_callback);
+// createServer(handler).listen(httpPort, start_callback);
 function handler(req, res) {
   setTimeout(() => {
     res.writeHead(200, "OK", { "Content-Type": "text/plain" });
     cheetahR.update("foo", { counter });
     res.write(String(counter));
-    counter += 1;`
+    counter += 1;
     res.end();
     return;
   }, 50);
 }
+
 function start_callback() {
   console.log("Start HTTP on port " + httpPort);
 }
